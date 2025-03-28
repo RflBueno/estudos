5cc2582851326d0e4a4cb230498cb746625b9b38
Rafael Bueno <rafael.bueno.c@hotmail.com>
2025-03-27 23:58:01 -0300

    fix problemas com o git e cache

--- Estudo JS/aula 12/script.js
+++
@@ -1,8 +0,0 @@
-let varA = 'A'; // B
-let varB = 'B'; // C
-let varC = 'C'; // A
-
-// Uma das solucoes mais atuais para o exercicio proposto no curso.
-[varA, varB, varC] = [varB, varC, varA];
-
-console.log(varA, varB, varC);
--- firstAPI/src/controllers/UserController.js
+++
@@ -1,9 +0,0 @@
-const users = require ('../mocks/users');
-
-
-module.exports = {
-  listUsers(request, response) {
-    response.writeHead(200, {'Content-type': 'application/json'});
-    response.end(JSON.stringify(users));
-  },
-};
